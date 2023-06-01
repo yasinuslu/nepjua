7b3cbb7587d30261baaf0a0f2298bccea8221fa4
Yasin Uslu <nepjua@gmail.com>
2023-06-02 01:35:45 +0300

    feat: configure home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -38,13 +38,100 @@
     homeDirectory = "/home/nepjua";
   };
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # Applications
+    google-chrome
+    microsoft-edge
+    # vscode
+    copyq
+    _1password
+    _1password-gui
+    qbittorrent
+    vlc
+
+    # Gnome
+    gnome.gnome-tweaks
+    guake
+    gnomeExtensions.advanced-alttab-window-switcher
+    gnomeExtensions.quick-settings-tweaker
+    gnomeExtensions.appindicator
+
+    # Command Line
+    wget
+    tldr
+    parsec-bin
+
+    # Fonts
+    # (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+
+    # From previous dotfiles
+    fish
+    jq
+    lsd
+    bat
+    fzf
+    starship
+    tmux
+    vim
+    nodejs
+    yarn
+    nodePackages.pnpm
+    python3Minimal
+
+    betterdiscordctl
+  ];
+
+  programs.git = {
+    enable = true;
+    diff-so-fancy.enable = true;
+  };
+
+  programs.vscode = {
+    enable = true;
+  };
+
+  programs.fish = {
+    enable = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/nepjua/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    XDG_DATA_DIRS = "/var/lib/flatpak/exports/share:$HOME/share/flatpak/exports/share";
+  };
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
