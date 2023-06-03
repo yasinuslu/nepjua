8909882513b39ceb468185331d46f54c8a9c411b
Yasin Uslu <nepjua@gmail.com>
2023-06-03 19:12:52 +0300

    feat: add fish plugins

--- home/nepjua/cli/fish/default.nix
+++ home/nepjua/cli/fish/default.nix
@@ -1,21 +1,121 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  programs.fish = {
+    enable = true;
+    plugins = [
+      {
+        name = "fisher";
+        src = pkgs.fetchFromGitHub {
+          owner = "jorgebucaran";
+          repo = "fisher";
+          rev = "master";
+          sha256 = "sha256-uWtEDpOrfQNGpS56WdoV13ggt4ESdsdM4gtPCcDpJyM=";
+        };
+      }
+      {
+        name = "edc-bass";
+        src = pkgs.fetchFromGitHub {
+          owner = "edc";
+          repo = "bass";
+          rev = "master";
+          sha256 = "sha256-uWtEDpOrfQNGpS56WdoV13ggt4ESdsdM4gtPCcDpJyM=";
+        };
+      }
+      {
+        name = "barnybug-docker-fish-completion";
+        src = pkgs.fetchFromGitHub {
+          owner = "barnybug";
+          repo = "docker-fish-completion";
+          rev = "master";
+          sha256 = "sha256-uWtEDpOrfQNGpS56WdoV13ggt4ESdsdM4gtPCcDpJyM=";
+        };
+      }
+      {
+        name = "jethrokuan-fzf";
+        src = pkgs.fetchFromGitHub {
+          owner = "jethrokuan";
+          repo = "fzf";
+          rev = "master";
+          sha256 = "sha256-uWtEDpOrfQNGpS56WdoV13ggt4ESdsdM4gtPCcDpJyM=";
+        };
+      }
+      {
+        name = "jorgebucaran-fishopts";
+        src = pkgs.fetchFromGitHub {
+          owner = "jorgebucaran";
+          repo = "fishopts";
+          rev = "master";
+          sha256 = "sha256-uWtEDpOrfQNGpS56WdoV13ggt4ESdsdM4gtPCcDpJyM=";
+        };
+      }
+      {
+        name = "jorgebucaran-fish-menu";
+        src = pkgs.fetchFromGitHub {
+          owner = "jorgebucaran";
+          repo = "fish-menu";
+          rev = "master";
+          sha256 = "sha256-uWtEDpOrfQNGpS56WdoV13ggt4ESdsdM4gtPCcDpJyM=";
+        };
+      }
+      {
+        name = "oh-my-fish-plugin-node-binpath";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-node-binpath";
+          rev = "master";
+          sha256 = "sha256-uWtEDpOrfQNGpS56WdoV13ggt4ESdsdM4gtPCcDpJyM=";
+        };
+      }
+      {
+        name = "oh-my-fish-plugin-osx";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-osx";
+          rev = "master";
+          sha256 = "sha256-uWtEDpOrfQNGpS56WdoV13ggt4ESdsdM4gtPCcDpJyM=";
+        };
+      }
+      {
+        name = "oh-my-fish-theme-bobthefish";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "theme-bobthefish";
+          rev = "master";
+          sha256 = "sha256-uWtEDpOrfQNGpS56WdoV13ggt4ESdsdM4gtPCcDpJyM=";
+        };
+      }
+      {
+        name = "jhillyerd-plugin-git";
+        src = pkgs.fetchFromGitHub {
+          owner = "jhillyerd";
+          repo = "plugin-git";
+          rev = "master";
+          sha256 = "sha256-uWtEDpOrfQNGpS56WdoV13ggt4ESdsdM4gtPCcDpJyM=";
+        };
+      }
+      {
+        name = "evanlucas-fish-kubectl-completions";
+        src = pkgs.fetchFromGitHub {
+          owner = "evanlucas";
+          repo = "fish-kubectl-completions";
+          rev = "master";
+          sha256 = "sha256-uWtEDpOrfQNGpS56WdoV13ggt4ESdsdM4gtPCcDpJyM=";
+        };
+      }
+    ];
+  };
+
   programs.starship = {
     enable = true;
     enableBashIntegration = true;
     enableFishIntegration = true;
 
     settings = {
-      add_newline = false;
-      format = lib.concatStrings [
-        "$line_break"
-        "$package"
-        "$line_break"
-        "$character"
-      ];
-      scan_timeout = 10;
-      character = {
-        success_symbol = "➜";
-        error_symbol = "➜";
+      git_branch = {
+        truncation_length = 24;
       };
     };
   };
