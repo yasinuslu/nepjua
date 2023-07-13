87d71376a240578596f47ab605b03b921c837712
Yasin Uslu <nepjua@gmail.com>
2023-07-13 15:13:04 +0300

    fix: remove alt-tab blacklist gst

--- darwin/programs/alt-tab.nix
+++ darwin/programs/alt-tab.nix
@@ -10,102 +10,11 @@
   system.defaults.menuExtraClock.ShowDayOfWeek = true;
   system.defaults.CustomUserPreferences = {
     "com.lwouis.alt-tab-macos" = {
-      blacklist = builtins.toJSON [
-        {
-          bundleIdentifier = "com.McAfee.McAfeeSafariHost";
-          hide = "1";
-          ignore = "0";
-        }
-        {
-          bundleIdentifier = "com.apple.finder";
-          hide = "2";
-          ignore = "0";
-        }
-        {
-          bundleIdentifier = "com.microsoft.rdc.macos";
-          hide = "0";
-          ignore = "2";
-        }
-        {
-          bundleIdentifier = "com.teamviewer.TeamViewer";
-          hide = "0";
-          ignore = "2";
-        }
-        {
-          bundleIdentifier = "org.virtualbox.app.VirtualBoxVM";
-          hide = "0";
-          ignore = "2";
-        }
-        {
-          bundleIdentifier = "com.parallels.";
-          hide = "0";
-          ignore = "2";
-        }
-        {
-          bundleIdentifier = "com.citrix.XenAppViewer";
-          hide = "0";
-          ignore = "2";
-        }
-        {
-          bundleIdentifier = "com.citrix.receiver.icaviewer.mac";
-          hide = "0";
-          ignore = "2";
-        }
-        {
-          bundleIdentifier = "com.nicesoftware.dcvviewer";
-          hide = "0";
-          ignore = "2";
-        }
-        {
-          bundleIdentifier = "com.vmware.fusion";
-          hide = "0";
-          ignore = "2";
-        }
-        {
-          bundleIdentifier = "com.apple.ScreenSharing";
-          hide = "0";
-          ignore = "2";
-        }
-        {
-          bundleIdentifier = "com.googlecode.iterm2";
-          hide = "2";
-          ignore = "0";
-        }
-        {
-          ignore = "0";
-          bundleIdentifier = "com.googlecode.iterm2";
-          hide = "2";
-        }
-        {
-          ignore = "0";
-          bundleIdentifier = "com.microsoft.Outlook";
-          hide = "2";
-        }
-        {
-          ignore = "0";
-          bundleIdentifier = "notion.id";
-          hide = "2";
-        }
-        {
-          ignore = "0";
-          bundleIdentifier = "com.microsoft.VSCode";
-          hide = "2";
-        }
-        {
-          ignore = "0";
-          bundleIdentifier = "us.zoom.xos";
-          hide = "2";
-        }
-        {
-          ignore = "0";
-          bundleIdentifier = "com.spotify.client";
-          hide = "2";
-        }
-      ];
       cursorFollowFocusEnabled = "false";
       holdShortcut = builtins.fromJSON "\"\\u2318\"";
       holdShortcut2 = builtins.fromJSON "\"\\u2318\"";
       mouseHoverEnabled = "true";
+      hideWindowlessApps = "true";
       nextWindowShortcut3 = builtins.fromJSON "\"\\u21e5\"";
       screensToShow = 1;
       showFullscreenWindows = 1;
