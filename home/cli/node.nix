45b32743fcd3d955068ca287ddf04516c0a555ab
Yasin Uslu <nepjua@gmail.com>
2023-11-26 20:54:51 +0300

    Remove bun

--- home/cli/node.nix
+++ home/cli/node.nix
@@ -7,7 +7,6 @@
   nodeBinDir = "${pkgs.nodejs}/bin";
   npmHome = "$HOME/.npm";
   pnpmHome = "$HOME/.nix-mutable/pnpm";
-  bunHome = "$HOME/.nix-mutable/bun";
   globalNodeModules = "$HOME/.nix-mutable/npm/node_modules";
 in {
   home.packages = with pkgs; [
@@ -20,28 +19,23 @@ in {
       rm -rf "${npmHome}"
       # Optional, when needed
       # rm -rf "${pnpmHome}"
-      # rm -rf "${bunHome}"
       rm -rf "${globalNodeModules}"
       mkdir -p "${globalNodeModules}"
       mkdir -p "${pnpmHome}"
-      mkdir -p "${bunHome}"
       echo "Done"
-      export PATH="${bunHome}/bin:${pnpmHome}:${nodeBinDir}:${globalNodeModules}/bin:$PATH"
+      export PATH="${pnpmHome}:${nodeBinDir}:${globalNodeModules}/bin:$PATH"
       export PNPM_HOME="${pnpmHome}"
-      export BUN_INSTALL="${bunHome}"
       npm config set prefix ${globalNodeModules}
-      echo "Installing bun..."
-      npm i -g bun
-      echo "Installing other global modules using bun..."
-      bun i -g bun pnpm yarn serve
+      npm i -g pnpm
+      echo "Installing other global modules using pnpm..."
+      pnpm i -g pnpm yarn serve
     '';
   };
 
-  home.extraPaths = ["${bunHome}/bin" "${pnpmHome}" "${globalNodeModules}/bin"];
+  home.extraPaths = ["${pnpmHome}" "${globalNodeModules}/bin"];
 
   home.sessionVariables = {
     NPM_CONFIG_PREFIX = globalNodeModules;
     PNPM_HOME = pnpmHome;
-    BUN_INSTALL = bunHome;
   };
 }
