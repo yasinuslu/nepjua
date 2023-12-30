fe7260a44fd909a8c420140d6b8cf831bfa231ba
Yasin Uslu <nepjua@gmail.com>
2023-12-30 17:01:27 +0300

    refactor: move things around

---
+++ machines/wsl/definition.nix
@@ -0,0 +1,48 @@
+{
+  self,
+  nixpkgs,
+  home-manager,
+  alejandra,
+  nix-index-database,
+  nixos-wsl,
+  ...
+} @ inputs:
+nixpkgs.lib.nixosSystem rec {
+  system = "x86_64-linux";
+  specialArgs = {
+    inherit inputs;
+    inherit self system;
+  }; # Pass flake inputs to our config
+
+  modules = [
+    {
+      environment.systemPackages = [
+        alejandra.defaultPackage."x86_64-linux"
+      ];
+    }
+    nixos-wsl.nixosModules.wsl
+    {
+      environment.systemPackages = with self.inputs.nix-alien.packages.${system}; [
+        nix-alien
+      ];
+      # Optional, needed for `nix-alien-ld`
+      programs.nix-ld.enable = true;
+    }
+    ../../machines/wsl/configuration.nix
+    nix-index-database.nixosModules.nix-index
+    {
+      programs.nix-index-database.comma.enable = true;
+      programs.nix-index.enable = true;
+      programs.command-not-found.enable = false;
+    }
+    home-manager.nixosModules.home-manager
+    {
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.nepjua = import ../../home/profiles/nixos-wsl;
+        extraSpecialArgs = {inherit inputs;};
+      };
+    }
+  ];
+}
