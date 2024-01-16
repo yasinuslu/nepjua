037e2447e49784e0b83ed7b8158109ed3023593c
Yasin Uslu <nepjua@gmail.com>
2024-01-16 15:44:37 +0300

    Remove mc user

--- darwin/raiden.nix
+++ darwin/raiden.nix
@@ -11,9 +11,6 @@
   users.users.nepjua.home = "/Users/nepjua";
   users.users.nepjua.shell = pkgs.fish;
 
-  # users.users.yasinuslu-mc.home = "/Users/yasinuslu-mc";
-  # users.users.yasinuslu-mc.shell = pkgs.fish;
-
   networking.hostName = "raiden";
   networking.computerName = "raiden";
 }
