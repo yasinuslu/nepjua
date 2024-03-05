eef2139317b52b0a3f1884cf2ae4219a73f620a9
personal Uslu <nepjua@gmail.com>
2024-03-05 13:57:54 +0300

    Apply gitignore to fzf

--- nix-config/home/src/cli/fzf.nix
+++ nix-config/home/src/cli/fzf.nix
@@ -8,18 +8,23 @@
     fd
   ];
 
+  programs.mcfly = {
+    enable = true;
+    enableFishIntegration = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    fzf = {
+      enable = true;
+    };
+  };
+
   programs.fzf = {
     enable = true;
     enableFishIntegration = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
 
-    defaultCommand = ''
-      fd --type f --strip-cwd-prefix --hidden --follow --exclude .git
-    '';
-
-    fileWidgetCommand = ''
-      fd --type f --strip-cwd-prefix --hidden --follow --exclude .git
-    '';
+    defaultCommand = "fd --type f --strip-cwd-prefix --hidden --follow";
+    fileWidgetCommand = "fd --type f --strip-cwd-prefix --hidden --follow";
   };
 }
