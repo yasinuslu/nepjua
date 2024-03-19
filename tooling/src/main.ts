a912ca7890bcfb0824535715ce02cde8ceee66d6
Yasin Uslu <nepjua@gmail.com>
2024-03-19 15:25:33 +0300

    Backup

--- tooling/src/main.ts
+++ tooling/src/main.ts
@@ -1,4 +1,10 @@
 import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 import { generate } from "./commands/generate/index.ts";
 
-await new Command().command("generate", generate).parse(Deno.args);
+console.log("test");
+
+await new Promise((resolve) => setTimeout(resolve, 1000));
+
+console.log("testtttt");
+
+// await new Command().command("generate", generate).parse(Deno.args);
