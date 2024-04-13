db0b36d389c7db69255ce99a01c103aa027cd1bc
Yasin Uslu <nepjua@gmail.com>
2024-04-14 00:12:43 +0300

    Update inputs

--- v2-nix-config/flake.nix
+++ v2-nix-config/flake.nix
@@ -5,15 +5,12 @@
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-release.url = "github:nixos/nixpkgs/nixos-23.11";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
-    darwin = {
-      url = "github:lnl7/nix-darwin/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    darwin.url = "github:lnl7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-colors.url = "github:misterio77/nix-colors";
 
@@ -22,10 +19,8 @@
 
     nix-alien.url = "github:thiagokokada/nix-alien";
 
-    flake-compat = {
-      url = "github:inclyc/flake-compat";
-      flake = false;
-    };
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
   };
 
   outputs = {
