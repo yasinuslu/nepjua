cd937999aa1c27efa2afe376e8b1e09021c5b7f2
Yasin Uslu <nepjua@gmail.com>
2024-04-15 00:39:51 +0300

    Update nix packages

--- v2-nix-config/flake.nix
+++ v2-nix-config/flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs-release.url = "github:nixos/nixpkgs/nixos-23.11";
 
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
