938b22e9fbe582b127bb81be9fafdb1310bea94e
Yasin Uslu <nepjua@gmail.com>
2024-04-18 23:53:31 +0300

    Rename base-os to base-system

--- v2-nix-config/nixosModules/bundles/general-desktop.nix
+++ v2-nix-config/nixosModules/bundles/general-desktop.nix
@@ -1,14 +1,8 @@
-{
-  inputs,
-  lib,
-  config,
-  pkgs,
-  ...
-}: {
+{lib, ...}: {
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.11";
 
-  myNixOS.base-os.enable = lib.mkDefault true;
+  myNixOS.base-system.enable = lib.mkDefault true;
   myNixOS.xserver-nvidia.enable = lib.mkDefault true;
   myNixOS.gnome.enable = lib.mkDefault true;
 }
