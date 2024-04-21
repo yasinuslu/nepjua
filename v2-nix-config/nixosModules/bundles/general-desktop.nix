e870869b17d0c0a57f4a22f30966d3724a120f85
Yasin Uslu <nepjua@gmail.com>
2024-04-21 21:40:26 +0300

    Use home-manager key

--- v2-nix-config/nixosModules/bundles/general-desktop.nix
+++ v2-nix-config/nixosModules/bundles/general-desktop.nix
@@ -2,5 +2,5 @@
   myNixOS.base-system.enable = lib.mkDefault true;
   myNixOS.xserver-nvidia.enable = lib.mkDefault true;
   myNixOS.gnome.enable = lib.mkDefault true;
-  myNixOS.home-users.enable = lib.mkDefault true;
+  myNixOS.home-manager.enable = lib.mkDefault true;
 }
