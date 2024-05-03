bb66f49e7c1d48bcc291be12b2a3b2e3d0e51824
Dev-Sezer <stogantemur8@gmail.com>
2024-05-03 13:36:48 +0300

    Make nix configurable

--- nix-config/modules/darwin/features/home-manager.nix
+++ nix-config/modules/darwin/features/home-manager.nix
@@ -67,7 +67,15 @@
               # '';
 
               programs.fish.interactiveShellInit = ''
-                eval "$(/opt/homebrew/bin/brew shellenv)"
+                function __init_homebrew
+                  if test -e /opt/homebrew/bin/brew
+                    eval (/opt/homebrew/bin/brew shellenv)
+                  end
+
+                  if test -e /usr/local/bin/brew
+                    eval (/usr/local/bin/brew shellenv)
+                  end
+                end
               '';
             })
             outputs.homeManagerModules.default
