df96c83106e9972a376d78f4ee8a1a4362ad2019
Yasin Uslu <nepjua@gmail.com>
2024-05-11 21:06:12 +0300

    Add kubernetes

--- nix-config/modules/darwin/features/home-manager.nix
+++ nix-config/modules/darwin/features/home-manager.nix
@@ -56,17 +56,35 @@
               home.username = name;
               home.homeDirectory = "/Users/${name}";
 
-              # programs.bash.interactiveShellInit = ''
-              #   eval "$(/opt/homebrew/bin/brew shellenv)"
-              #   . /etc/profiles/per-user/${name}/etc/profile.d/*
-              # '';
+              programs.zsh.initExtra = ''
+                __init_homebrew() {
+                  if [[ -e /opt/homebrew/bin/brew ]]; then
+                    eval $(/opt/homebrew/bin/brew shellenv)
+                  fi
 
-              # programs.zsh.interactiveShellInit = ''
-              #   eval "$(/opt/homebrew/bin/brew shellenv)"
-              #   . /etc/profiles/per-user/${name}/etc/profile.d/*
-              # '';
+                  if [[ -e /usr/local/bin/brew ]]; then
+                    eval $(/usr/local/bin/brew shellenv)
+                  fi
+                }
 
-              programs.fish.interactiveShellInit = ''
+                __init_homebrew
+              '';
+
+              programs.bash.initExtra = ''
+                __init_homebrew() {
+                  if [[ -e /opt/homebrew/bin/brew ]]; then
+                    eval $(/opt/homebrew/bin/brew shellenv)
+                  fi
+
+                  if [[ -e /usr/local/bin/brew ]]; then
+                    eval $(/usr/local/bin/brew shellenv)
+                  fi
+                }
+
+                __init_homebrew
+              '';
+
+              programs.fish.shellInit = ''
                 function __init_homebrew
                   if test -e /opt/homebrew/bin/brew
                     eval (/opt/homebrew/bin/brew shellenv)
