8570cdf3ffe5fa71cbd1ad4a9f56da3ac089249a
Halil Ertekin <halil@ertekin.me>
2024-06-10 12:25:16 +0200

    Add halil

---
+++ nix-config/hosts/halil/configuration.nix
@@ -0,0 +1,21 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+{...}: {
+  networking.hostName = "halil-mbp";
+  networking.computerName = "Halils-MBP";
+
+  myNixOS = {
+    bundles.darwin-desktop.enable = true;
+
+    users = {
+      nepjua = {
+        userConfig = {...}: {
+          programs.git.userName = "Halil Ertekin";
+          programs.git.userEmail = "halil@ertekin.me";
+        };
+        userSettings = {};
+      };
+    };
+  };
+}
