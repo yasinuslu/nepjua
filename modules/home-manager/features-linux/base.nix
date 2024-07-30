d23e37bf16e0e1bac6e09f617f2d52821fd8a8ff
Yasin Uslu <nepjua@gmail.com>
2024-07-30 22:56:29 +0300

    Use busybox intead of toybox

--- modules/home-manager/features-linux/base.nix
+++ modules/home-manager/features-linux/base.nix
@@ -8,7 +8,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    toybox
+    busybox
     vlc
     copyq
     parsec-bin
