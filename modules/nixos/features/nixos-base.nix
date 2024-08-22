1aba34ce14dda02daf990862105a18e26f9b3096
Yasin Uslu <nepjua@gmail.com>
2024-08-22 17:10:23 +0300

    Add memtest to bootloader

--- modules/nixos/features/nixos-base.nix
+++ modules/nixos/features/nixos-base.nix
@@ -69,5 +69,6 @@
   services.getty.autologinUser = "nepjua";
 
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.memtest86.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 }
