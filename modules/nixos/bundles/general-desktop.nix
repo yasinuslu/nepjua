f66353373888ddf5d5ec3e54fafbd4f2b4ca4994
Yasin Uslu <nepjua@gmail.com>
2024-10-05 22:36:03 +0300

    Switch to docker

--- modules/nixos/bundles/general-desktop.nix
+++ modules/nixos/bundles/general-desktop.nix
@@ -3,8 +3,8 @@
     qemu-guest.enable = false;
     gaming.enable = false;
 
-    docker.enable = false;
-    podman.enable = true;
+    docker.enable = true;
+    podman.enable = false;
 
     # There seems to be an issue with the virtualbox
     virtualbox.enable = false;
