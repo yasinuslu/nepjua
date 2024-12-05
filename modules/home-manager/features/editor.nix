00ae6bb217f442cc1b9e8af487ded1cd13e26ed7
Yasin Uslu <nepjua@gmail.com>
2024-12-05 11:56:51 +0300

    Keep mscode

--- modules/home-manager/features/editor.nix
+++ modules/home-manager/features/editor.nix
@@ -4,4 +4,19 @@
     CODE_EDITOR = "cursor";
     REACT_EDITOR = "cursor";
   };
+
+  programs.zsh.initExtraFirst = ''
+    alias code="cursor"
+    alias mscode="/opt/homebrew/bin/code"
+  '';
+
+  programs.bash.profileExtra = ''
+    alias code="cursor"
+    alias mscode="/opt/homebrew/bin/code"
+  '';
+
+  programs.fish.loginShellInit = ''
+    alias code="cursor"
+    alias mscode="/opt/homebrew/bin/code"
+  '';
 }
