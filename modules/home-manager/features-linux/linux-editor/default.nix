d99df88b05569b677a8abfb8630cde80762407fe
Yasin Uslu <nepjua@gmail.com>
2024-12-31 22:45:31 +0300

    Fix build

--- modules/home-manager/features-linux/linux-editor/default.nix
+++ modules/home-manager/features-linux/linux-editor/default.nix
@@ -1,15 +1,11 @@
-{
-  pkgs,
-  stdenv,
-  ...
-}: {
+{pkgs, ...}: {
   home.packages = with pkgs;
     [
       vscode
       zed-editor
     ]
     ++ (
-      if stdenv.system == "x86_64-linux"
+      if pkgs.stdenv.system == "x86_64-linux"
       then [code-cursor]
       else []
     );
