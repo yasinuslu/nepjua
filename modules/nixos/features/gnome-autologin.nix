9f352ad3fd7148224d0706f20c7734cde0e91352
Yasin Uslu <nepjua@gmail.com>
2025-01-01 16:30:02 +0300

    Fix build

--- modules/nixos/features/gnome-autologin.nix
+++ modules/nixos/features/gnome-autologin.nix
@@ -1,7 +1,7 @@
-{...}: {
+{config, ...}: {
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "proxmox";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = config.myNixOS.mainUser;
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
