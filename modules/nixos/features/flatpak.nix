f3ffc92bf8516c89a5119fc2d6d64b66bf5c72d2
Yasin Uslu <nepjua@gmail.com>
2025-01-02 04:34:04 +0300

    Fix build

--- modules/nixos/features/flatpak.nix
+++ modules/nixos/features/flatpak.nix
@@ -5,20 +5,19 @@
   # Add required packages
   environment.systemPackages = with pkgs; [
     flatpak
-    gnome-software # Now at top-level
-    gnome-software-plugin-flatpak
+    gnome-software
   ];
-
-  # Add Flathub repository after installation
-  system.activationScripts.flatpak-init = ''
-    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-  '';
 
   # Configure XDG portal for better Flatpak integration
   xdg.portal = {
     enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-    ];
+    extraPortals = [pkgs.xdg-desktop-portal-gtk];
+    # Set GTK portal as default
+    config.common.default = "gtk";
   };
+
+  # Add Flathub repository after installation
+  system.activationScripts.flatpak-init = ''
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  '';
 }
