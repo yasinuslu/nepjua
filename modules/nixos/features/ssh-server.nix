f2649cc8f45514ed0d00880db57feb1d947b9a74
Yasin Uslu <nepjua@gmail.com>
2025-01-02 04:17:03 +0300

    Use zsh in ssh

--- modules/nixos/features/ssh-server.nix
+++ modules/nixos/features/ssh-server.nix
@@ -1,11 +1,11 @@
-{pkgs, ...}: {
+{...}: {
   services.openssh = {
     enable = true;
     allowSFTP = true;
     settings = {
       PermitRootLogin = "yes";
       PasswordAuthentication = true;
-      ForceCommand = "${pkgs.bash.outPath}/bin/bash";
+      ForceCommand = "zsh";
     };
   };
 }
