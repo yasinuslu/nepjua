de7218ec92985090733ce0df1ba9ee07093b40c5
Yasin Uslu <nepjua@gmail.com>
2025-01-03 03:42:02 +0300

    Fix build

--- modules/nixos/bundles/proxmox-guest/hardware-configuration.nix
+++ modules/nixos/bundles/proxmox-guest/hardware-configuration.nix
@@ -32,12 +32,6 @@
     fsType = "ext4";
   };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/boot";
-    fsType = "vfat";
-    options = ["fmask=0077" "dmask=0077"];
-  };
-
   swapDevices = [
     {device = "/dev/disk/by-label/swap";}
   ];
