0dcd3d6ac8c2b1325321884bd4016d212b381187
Yasin Uslu <nepjua@gmail.com>
2025-01-18 22:27:53 +0300

    Add grub

---
+++ modules/nixos/bundles/minimal.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  myNixOS = {
+    bundles.general-desktop.enable = lib.mkForce false;
+
+    flatpak.enable = lib.mkForce false;
+    appimage.enable = lib.mkForce false;
+    xserver-nvidia.enable = lib.mkForce false;
+    gaming.enable = lib.mkForce false;
+  };
+
+  # Basic system configuration
+  system.stateVersion = "23.11"; # Adjust to your current state version
+}
