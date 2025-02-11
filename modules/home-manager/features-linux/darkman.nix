00f6347749365242ed7e75793d9f711fc9b326df
Yasin Uslu <nepjua@gmail.com>
2025-02-12 01:53:34 +0300

    Do not use geoclue

--- modules/home-manager/features-linux/darkman.nix
+++ modules/home-manager/features-linux/darkman.nix
@@ -1,4 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  services.darkman.enable = true;
+  services.darkman = {
+    enable = true;
+    settings = {
+      lat = 39.9032594;
+      lng = 32.5976167;
+      usegeoclue = false;
+    };
+    lightModeScripts = {
+      gtk-theme = ''
+        ${pkgs.dconf}/bin/dconf write \
+            /org/gnome/desktop/interface/color-scheme "'prefer-light'"
+      '';
+    };
+    darkModeScripts = {
+      gtk-theme = ''
+        ${pkgs.dconf}/bin/dconf write \
+            /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+      '';
+    };
+  };
 }
