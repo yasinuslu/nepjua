556e21b4460d751b8a54b737fbaa5246e9aee082
Yasin Uslu <nepjua@gmail.com>
2025-02-12 02:12:48 +0300

    Enable zfs pools

--- modules/nixos/features/zfs.nix
+++ modules/nixos/features/zfs.nix
@@ -1,5 +1,19 @@
 { lib, ... }:
 {
   boot.supportedFilesystems = [ "zfs" ];
-  boot.zfs.forceImportRoot = lib.mkDefault false;
+  boot.initrd.supportedFilesystems = {
+    zfs = lib.mkDefault true;
+  };
+  boot.zfs = {
+    enabled = lib.mkDefault true;
+    forceImportRoot = lib.mkDefault false;
+    extraPools = [ "zpool" ];
+  };
+
+  services.zfs = {
+    autoReplication.enable = true;
+    autoScrub.enable = true;
+    autoSnapshot.enable = true;
+    trim.enable = true;
+  };
 }
