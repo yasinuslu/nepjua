074c9abd656b3eebe23490e7987da16369915ffc
Yasin Uslu <nepjua@gmail.com>
2025-02-15 04:25:52 +0300

    Use only 1 file for vm-host

--- modules/nixos/features/vm-host.nix
+++ modules/nixos/features/vm-host.nix
@@ -19,4 +19,32 @@
   services.spice-vdagentd.enable = true;
 
   programs.virt-manager.enable = true;
+
+  # Add additional useful packages
+  environment.systemPackages = with pkgs; [
+    virt-viewer
+    spice-gtk
+    win-virtio
+    win-spice
+  ];
+
+  boot = {
+    kernelParams = [
+      "amd_iommu=on"
+      "iommu=pt"
+    ];
+    initrd.kernelModules = [
+      "vfio_pci"
+      "vfio_iommu_type1"
+      "vfio_virqfd"
+    ];
+    blacklistedKernelModules = [
+      "nouveau"
+      "nvidiafb"
+      "nvidia-gpu"
+    ];
+    extraModprobeConfig = ''
+      options vfio-pci ids=0000:01:00.0,0000:01:00.1 # Replace with your GPU PCI IDs
+    '';
+  };
 }
