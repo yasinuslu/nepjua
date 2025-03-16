b1e26770c892688a18641fbb3357b2d033f9b382
Yasin Uslu <nepjua@gmail.com>
2025-03-16 06:38:45 +0300

    Combine options and config

--- modules/flake/biseyler.nix
+++
@@ -1,5 +0,0 @@
-{
-  yasin = {
-    selam = "aleykum selam";
-  };
-}
--- modules/flake/config.nix
+++ modules/flake/config.nix
@@ -1,11 +1,40 @@
-# Global configuration for this repo
-#
-# See ./modules/flake-parts/config-module.nix for schema
+{ lib, ... }:
+let
+  userSubmodule = lib.types.submodule {
+    options = {
+      username = lib.mkOption {
+        type = lib.types.str;
+        description = "Your username as shown by `id -un`";
+      };
+      githubUsername = lib.mkOption {
+        type = lib.types.str;
+        description = "Your GitHub username";
+      };
+      fullname = lib.mkOption {
+        type = lib.types.str;
+        description = "Your full name for use in Git config";
+      };
+      email = lib.mkOption {
+        type = lib.types.str;
+        description = "Your email for use in Git config";
+      };
+    };
+  };
+in
 {
-  me = {
-    username = "nepjua";
-    githubUsername = "yasinuslu";
-    fullname = "Yasin Uslu";
-    email = "nepjua@gmail.com";
+  options = {
+    me = lib.mkOption {
+      default = { };
+      type = userSubmodule;
+    };
+  };
+
+  config = {
+    me = {
+      username = "nepjua";
+      githubUsername = "yasinuslu";
+      fullname = "Yasin Uslu";
+      email = "nepjua@gmail.com";
+    };
   };
 }
