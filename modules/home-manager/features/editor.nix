5677fdbaa7bb8c823ed0dcbb19f57e3f205c3e56
Yasin Uslu <nepjua@gmail.com>
2025-04-07 22:35:04 +0300

    Add editorBeta package

--- modules/home-manager/features/editor.nix
+++ modules/home-manager/features/editor.nix
@@ -48,9 +48,56 @@ let
       vim "''${ARGS_WITHOUT_WAIT[@]}"
     fi
   '';
+  editorBetaPackage = pkgs.writeShellScriptBin "eb" ''
+    is_remote_vscode_path() {
+      if [[ "$1" == */remote-cli/* ]]; then
+        return 0
+      fi
+
+      return 1
+    }
+
+    is_available() {
+      if [ -z "$1" ]; then
+        return 1
+      fi
+
+      return 0
+    }
+
+    CODE_PATH="$(command -v code)"
+    CURSOR_PATH="$(command -v cursor)"
+    VIM_PATH="$(command -v vim)"
+    FULL_PWD="$( realpath "$PWD" )"
+
+    if is_remote_vscode_path "$CODE_PATH"; then
+      code-insiders "$@"
+    elif is_remote_vscode_path "$CURSOR_PATH"; then
+      cursor "$@"
+    elif [[ "$FULL_PWD" == */mastercontrol/* ]]; then
+      code-insiders "$@"
+    elif [[ "$FULL_PWD" == */yasinuslu/* ]]; then
+      cursor "$@"
+    elif is_available "$CURSOR_PATH"; then
+      cursor "$@"
+    elif is_available "$VIM_PATH"; then
+      ARGS_WITHOUT_WAIT=()
+
+      for arg in "$@"; do
+        if [[ "$arg" != "--wait" ]]; then
+          ARGS_WITHOUT_WAIT+=("$arg")
+        fi
+      done
+
+      vim "''${ARGS_WITHOUT_WAIT[@]}"
+    fi
+  '';
 in
 {
-  home.packages = [ editorPackage ];
+  home.packages = [
+    editorPackage
+    editorBetaPackage
+  ];
 
   home.sessionVariables = {
     EDITOR = "e --wait";
