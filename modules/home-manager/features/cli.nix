e8439fdb8e3116793a3577bae90a5d8ab9541173
Yasin Uslu <nepjua@gmail.com>
2025-06-12 01:05:44 +0300

    Add nepjua cli

--- modules/home-manager/features/cli.nix
+++ modules/home-manager/features/cli.nix
@@ -4,9 +4,15 @@ let
     export NEPJUA_ROOT="$NEPJUA_ROOT:-$HOME/code/nepjua"
     deno run -A -c "$NEPJUA_ROOT"/deno.jsonc "$NEPJUA_ROOT"/cli/main.ts "$@"
   '';
+
+  nepjuaCliDebug = pkgs.writeShellScriptBin "nep-debug" ''
+    export NEPJUA_ROOT="$NEPJUA_ROOT:-$HOME/code/nepjua"
+    deno run -A -c "$NEPJUA_ROOT"/deno.jsonc --inspect-brk "$NEPJUA_ROOT"/cli/main.ts "$@"
+  '';
 in
 {
   home.packages = [
     nepjuaCli
+    nepjuaCliDebug
   ];
 }
