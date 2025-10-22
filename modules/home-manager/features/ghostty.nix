589996042304637d9083626f84fa912372827b8c
Yasin Uslu <nepjua@gmail.com>
2025-10-22 23:43:25 +0300

    Update ghostty config

--- modules/home-manager/features/ghostty.nix
+++ modules/home-manager/features/ghostty.nix
@@ -3,10 +3,22 @@
   # Ghostty is installed via homebrew on macOS (see homebrew-minimal.nix)
   # We only manage the configuration here
   xdg.configFile."ghostty/config".text = ''
+    # Theme
+    theme = "Monokai Remastered"
+
     # Font
-    font-size = 14
+    font-family = "JetBrainsMono Nerd Font Mono"
+    font-size = 15
 
     # Keybindings
     keybind = global:alt+backquote=toggle_quick_terminal
+
+    shell-integration = fish
+
+    command = "/run/current-system/sw/bin/fish -l"
   '';
+
+  myHomeManager.paths = [
+    "$GHOSTTY_BIN_DIR"
+  ];
 }
