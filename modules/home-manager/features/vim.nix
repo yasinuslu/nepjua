e2b57f31f72a1177dc65e51cbf5fddda177addc7
Yasin Uslu <nepjua@gmail.com>
2026-02-26 10:23:54 -0600

    Make sure to install pngpaste

--- modules/home-manager/features/vim.nix
+++ modules/home-manager/features/vim.nix
@@ -1,17 +1,26 @@
-{ inputs, lib, pkgs, ... }:
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
 {
   home.packages = [
-    (let
-      baseConfig = inputs.khanelivim.nixvimConfigurations.${pkgs.system}.khanelivim;
-      extendedConfig = baseConfig.extendModules {
-        modules = [
-          {
-            # Disable specific plugins
-            plugins.neotest.enable = lib.mkForce false;
-          }
-        ];
-      };
-    in extendedConfig.config.build.package)
+    (
+      let
+        baseConfig = inputs.khanelivim.nixvimConfigurations.${pkgs.system}.khanelivim;
+        extendedConfig = baseConfig.extendModules {
+          modules = [
+            {
+              # Disable specific plugins
+              plugins.neotest.enable = lib.mkForce false;
+            }
+          ];
+        };
+      in
+      extendedConfig.config.build.package
+    )
+    pkgs.pngpaste
   ];
 
   home.shellAliases = {
